3953d424fd71b12ae6cf81fb0c28508387ffb1e1
Fernando747 <fernandojcr5@gmail.com>
2022-01-29 03:56:45 -0600

    Text Iterator and TMP

---
+++ ProyectPaps/Assets/Tools/TextIterator.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextIterator : MonoBehaviour
+{
+    private Coroutine corIterateText;
+    private TextMeshProUGUI textMesh;
+    private bool finished = false;
+
+    [Header("Iteration speed")]
+    [SerializeField] private float textSpeed = 0.5f;
+
+    public void Awake()
+    {
+        textMesh = GetComponent<TextMeshProUGUI>();
+    }
+
+    public void OnEnable()
+    {
+        finished = false;
+        textMesh.maxVisibleCharacters = 0;
+        Show();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(2) && finished == false)
+        {
+            skip();
+        }
+    }
+
+    private void skip()
+    {
+        if (corIterateText != null)
+        {
+            StopCoroutine(corIterateText);
+        }
+        textMesh.maxVisibleCharacters = textMesh.text.Length;
+        finished = true;
+    }
+
+    public void Show()
+    {
+        if (corIterateText != null)
+        {
+            StopCoroutine(corIterateText);
+        }
+        finished = false;
+        corIterateText = StartCoroutine(CorIterateText(textMesh));
+    }
+
+    IEnumerator CorIterateText(TextMeshProUGUI myTextMesh)
+    {
+        for (int i = 0; i < myTextMesh.text.Length; i++)
+        {
+            myTextMesh.maxVisibleCharacters++;
+            yield return new WaitForSeconds(textSpeed);
+        }
+        finished = true;
+    }
+}
